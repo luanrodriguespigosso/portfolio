2db13a3873ba2e38592a3045049f2418417b50c5
luan <luan_naul90@hotmail.com>
2022-01-12 20:22:34 -0300

    Formanto email enviado pelo formulário

--- assets/php/send.php
+++ assets/php/send.php
@@ -7,12 +7,27 @@
     ini_set( 'display_erros', 1 );
     error_reporting( E_ALL );
 
-    $from = "contato@luanrodriguespigosso.com.br";
-    $to = "luanrodriguespigosso@gmail.com";
     $subject = "E-mail do site - ".$name;
-    $message = 'Nome: '.$name.' --- Menssagem: '.$msg.' --- Email: '.$email;
-    $headers = "From:" . $from;
-    mail($to,$subject,$message, $headers);
+    $to = "luanrodriguespigosso@gmail.com";
+    
+    $message = "
+    <html>
+    <body>
+    <h3>Olá, me chamo $name.</h3>
+    <br>
+    <p>$msg</p>
+    <br>
+    <p>E-mail: $email</p>
+    </body>
+    </html>
+    ";
+    
+    $headers[] = 'MIME-Version: 1.0';
+    $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+    $headers[] = 'From: '.$name.' <'.$email.'>';
+
+    mail($to,$subject,$message, implode("\r\n", $headers));
 
     header('Location: ../../index.html');
+    
 ?>
