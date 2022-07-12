3cfd95f356ad6795cc61039e997b853861ad583e
luan <luan_naul90@hotmail.com>
2022-07-12 18:29:41 -0300

    fix: Removido seção contato, blog e códigos desnecessários

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,37 +1,20 @@
 // menu hamburguer START
-const btnMobile = document.getElementById('btn-mobile');
+const btnMobile = document.getElementById("btn-mobile");
 
 function toggleMenu(event) {
-    if (event.type === 'touchstart') event.preventDefault();
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
 
-    const active = nav.classList.contains('active');
-    event.currentTarget.setAttribute('aria-expanded', active);
-    if (active) {
-        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-    } else {
-        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-    }
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+  }
 }
 
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
 // menu hamburguer END
-
-/* Start valida email*/
-function validaEmail(email) {
-    var validate = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
-    return validate(email)
-}
-/* End valida email*/
-
-// contador de caracteres formulário START
-$(document).on("input", "#msg" , function() {
-    var limit = 500;
-    var total = $(this).val().length;
-    var remaining = limit - total;
-
-    $("#counter").text(remaining);
-});
-// contador de caracteres formulário END
